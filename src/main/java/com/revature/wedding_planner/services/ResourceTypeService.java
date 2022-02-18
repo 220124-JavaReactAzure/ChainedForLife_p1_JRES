fc7f5b1b14920fd6254c84d3f7e59ba859be3f53
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-18 11:43:26 -0800

    First impl of Resource and ResourceType Services

--- src/main/java/com/revature/wedding_planner/services/ResourceTypeService.java
+++ src/main/java/com/revature/wedding_planner/services/ResourceTypeService.java
@@ -1,5 +1,32 @@
 package com.revature.wedding_planner.services;
 
-public class ResourceTypeService {
+import java.util.List;
 
+import com.revature.wedding_planner.dao.ResourceTypeDAO;
+import com.revature.wedding_planner.models.ResourceType;
+
+public class ResourceTypeService {
+	
+	private final ResourceTypeDAO resourceTypeDAO;
+	
+	public ResourceTypeService(ResourceTypeDAO resourceTypeDAO) {
+		this.resourceTypeDAO = resourceTypeDAO;
+	}
+	
+	public boolean addResourceType(ResourceType resourceType) {
+		return resourceTypeDAO.addResourceType(resourceType);
+	}
+	
+	public List<ResourceType> getAllResourceTypes() {
+		return resourceTypeDAO.getAllResourceTypes();
+	}
+	
+	public ResourceType getResourceTypeByID(int id) {
+		return resourceTypeDAO.getResourceTypeByID(id);
+	}
+	
+	// TODO implement this in DAO
+	public void deleteResourceType(int id) {
+		resourceTypeDAO.deleteResourceType(id);
+	}
 }
