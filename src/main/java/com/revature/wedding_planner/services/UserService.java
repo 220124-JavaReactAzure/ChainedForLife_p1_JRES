d7aec98fb2f2935cc0bb0312efac5aef8813b85d
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-18 08:24:09 -0800

    User and UserType services first implementation in

--- src/main/java/com/revature/wedding_planner/services/UserService.java
+++ src/main/java/com/revature/wedding_planner/services/UserService.java
@@ -1,5 +1,32 @@
 package com.revature.wedding_planner.services;
 
+import java.util.List;
+
+import com.revature.wedding_planner.dao.UserDAO;
+import com.revature.wedding_planner.models.User;
+
 public class UserService {
 
+	private final UserDAO userDAO;
+	
+	public UserService(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	public boolean addUser(User user) {
+		return userDAO.addUser(user);
+	}
+	
+	public List<User> getAllUsers() {
+		return userDAO.getAllUsers();
+	}
+	
+	public User getUserByID(int id) {
+		return userDAO.getUserByID(id);
+	}
+	
+	// TODO implement this in DAO
+	public void deleteUser(int id) {
+		userDAO.deleteUser(id);
+	}
 }
--- src/main/java/com/revature/wedding_planner/services/UserTypeService.java
+++ src/main/java/com/revature/wedding_planner/services/UserTypeService.java
@@ -1,5 +1,33 @@
 package com.revature.wedding_planner.services;
 
+import java.util.List;
+
+import com.revature.wedding_planner.dao.UserTypeDAO;
+import com.revature.wedding_planner.models.UserType;
+
 public class UserTypeService {
+	
+	private final UserTypeDAO userTypeDAO;
+	
+	public UserTypeService(UserTypeDAO userTypeDAO) {
+		this.userTypeDAO = userTypeDAO;
+	}
+	
+	public boolean addUserType(UserType userType) {
+		return userTypeDAO.addUserType(userType);
+	}
+	
+	public List<UserType> getAllUserTypes() {
+		return userTypeDAO.getAllUserTypes();
+	}
+	
+	public UserType getUserTypeByID(int id) {	
+		return userTypeDAO.getUserTypeByID(id);
+	}
+	
+	//TODO implement in DAO
+	public void deleteUserType(int id) {
+		userTypeDAO.deleteUserType(id);
+	}
 
 }
