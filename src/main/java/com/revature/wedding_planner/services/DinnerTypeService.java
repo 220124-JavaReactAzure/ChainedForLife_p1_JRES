1f7760adabb7c1eb5ea3e8f1ae3c1cb8779f16cf
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-18 12:10:49 -0800

    First impl of DinnerTypeService

--- src/main/java/com/revature/wedding_planner/services/DinnerTypeService.java
+++ src/main/java/com/revature/wedding_planner/services/DinnerTypeService.java
@@ -1,5 +1,32 @@
 package com.revature.wedding_planner.services;
 
+import java.util.List;
+
+import com.revature.wedding_planner.dao.DinnerTypeDAO;
+import com.revature.wedding_planner.models.DinnerType;
+
 public class DinnerTypeService {
 
+	private final DinnerTypeDAO dinnerTypeDAO;
+	
+	public DinnerTypeService(DinnerTypeDAO dinnerTypeDAO) {
+		this.dinnerTypeDAO = dinnerTypeDAO;
+	}
+	
+	public boolean addDinnerType(DinnerType dinnerType) {
+		return dinnerTypeDAO.addDinnerType(dinnerType);
+	}
+	
+	public List<DinnerType> getAllDinnerTypes() {
+		return dinnerTypeDAO.getAllDinnerTypes();
+	}
+	
+	public DinnerType getDinnerTypeByID(int id) {
+		return dinnerTypeDAO.getDinnerTypeByID(id);
+	}
+	
+	// TODO implement this in DAO
+	public void deleteDinnerType(int id) {
+		dinnerTypeDAO.deleteDinnerType(id);
+	}
 }
