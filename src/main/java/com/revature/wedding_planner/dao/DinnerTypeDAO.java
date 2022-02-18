f014ea8fc965a9229ffc6b9127a3e3b87697c580
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-18 11:59:55 -0800

    First impl of DinnerTypeDAO

--- src/main/java/com/revature/wedding_planner/dao/DinnerTypeDAO.java
+++ src/main/java/com/revature/wedding_planner/dao/DinnerTypeDAO.java
@@ -1,5 +1,63 @@
 package com.revature.wedding_planner.dao;
 
-public class DinnerTypeDAO {
+import java.io.IOException;
+import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.revature.wedding_planner.models.DinnerType;
+import com.revature.wedding_planner.util.HibernateUtil;
+
+public class DinnerTypeDAO {
+	public boolean addDinnerType(DinnerType dinnerType) {
+		try {
+			Session session = HibernateUtil.getSession();
+			session.save(dinnerType);
+			return true;
+		} catch (HibernateException | IOException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
+
+	public List<DinnerType> getAllDinnerTypes() {
+		try {
+			Session session = HibernateUtil.getSession();
+			@SuppressWarnings("unchecked")
+			List<DinnerType> dinnerTypes = session.createQuery("FROM DinnerType").list();
+			return dinnerTypes;
+		} catch (HibernateException | IOException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
+
+	public DinnerType getDinnerTypeByID(int id) {
+		try {
+			Session session = HibernateUtil.getSession();
+			DinnerType dinnerType = session.get(DinnerType.class, id);
+			return dinnerType;
+		} catch (HibernateException | IOException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
+
+	// TODO implement this for it's not complete
+	public void deleteDinnerType(int id) {
+		try {
+			Session session = HibernateUtil.getSession();
+		} catch (HibernateException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
 }
