1ccb8558f8462c783476cc4f1c6670eb3bb4b37d
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-18 19:36:34 -0800

    PlusOne Model first impl

--- src/main/java/com/revature/wedding_planner/models/PlusOne.java
+++ src/main/java/com/revature/wedding_planner/models/PlusOne.java
@@ -1,5 +1,97 @@
 package com.revature.wedding_planner.models;
 
-public class PlusOne {
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@Table(name= "weddings")
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property = "id")
+public class PlusOne {
+	@Id
+	@JoinColumn(name = "attendee_id", nullable = false)
+	private int attendeeID;
+	
+	@Id
+	@JoinColumn(name="Wedding_id", nullable = false)
+	private int weddingID;
+	
+	@JoinColumn(name="dinner_type_id", nullable = false)
+	private int dinnerTypeID;
+	
+	// Constructors
+
+	public PlusOne() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlusOne(int attendeeID, int weddingID, int dinnerTypeID) {
+		super();
+		this.attendeeID = attendeeID;
+		this.weddingID = weddingID;
+		this.dinnerTypeID = dinnerTypeID;
+	}
+	
+	// Getters and Setters
+
+	public int getAttendeeID() {
+		return attendeeID;
+	}
+
+	public void setAttendeeID(int attendeeID) {
+		this.attendeeID = attendeeID;
+	}
+
+	public int getWeddingID() {
+		return weddingID;
+	}
+
+	public void setWeddingID(int weddingID) {
+		this.weddingID = weddingID;
+	}
+
+	public int getDinnerTypeID() {
+		return dinnerTypeID;
+	}
+
+	public void setDinnerTypeID(int dinnerTypeID) {
+		this.dinnerTypeID = dinnerTypeID;
+	}
+	
+	// Methods
+
+	@Override
+	public String toString() {
+		return "PlusOne [attendeeID=" + attendeeID + ", weddingID=" + weddingID + ", dinnerTypeID=" + dinnerTypeID
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(attendeeID, dinnerTypeID, weddingID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlusOne other = (PlusOne) obj;
+		return attendeeID == other.attendeeID && dinnerTypeID == other.dinnerTypeID && weddingID == other.weddingID;
+	}
+	
+	
 }
