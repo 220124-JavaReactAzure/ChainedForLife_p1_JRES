0edb9d5794a81e57985795c59b257d209eee3e33
aaescobar <47513774+aaescobar@users.noreply.github.com>
2022-02-19 19:10:42 -0500

    Setup servlets did not saved previously work

--- src/main/java/com/revature/wedding_planner/web/servlets/DinnerTypeServlet.java
+++ src/main/java/com/revature/wedding_planner/web/servlets/DinnerTypeServlet.java
@@ -1,7 +1,17 @@
 package com.revature.wedding_planner.web.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class DinnerTypeServlet extends HttpServlet{
-
+ 
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Init has been called for TestServlet");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("TestServlet Destroyer");
+	}
 }
--- src/main/java/com/revature/wedding_planner/web/servlets/ResourceServlet.java
+++ src/main/java/com/revature/wedding_planner/web/servlets/ResourceServlet.java
@@ -1,7 +1,16 @@
 package com.revature.wedding_planner.web.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class ResourceServlet extends HttpServlet{
-
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Init has been called for TestServlet");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("TestServlet Destroyer");
+	}
 }
