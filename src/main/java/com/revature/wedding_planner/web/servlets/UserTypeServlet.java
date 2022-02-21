b44d363b406d24a61ad4d845688116a945a8eea0
aaescobar <47513774+aaescobar@users.noreply.github.com>
2022-02-21 11:59:51 -0500

    pusing files before pulling from main

--- src/main/java/com/revature/wedding_planner/web/servlets/UserTypeServlet.java
+++ src/main/java/com/revature/wedding_planner/web/servlets/UserTypeServlet.java
@@ -1,8 +1,10 @@
 package com.revature.wedding_planner.web.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+@WebServlet("/usertype")
 public class UserTypeServlet extends HttpServlet{
 	@Override
 	public void init() throws ServletException {
