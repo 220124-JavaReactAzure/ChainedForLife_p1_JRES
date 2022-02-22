62da2873fada5e456c5477c78fcfb9443808ce03
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-22 08:49:53 -0800

    RentedResource DAO and Service added and fixed

--- src/main/java/com/revature/wedding_planner/services/RentedResourceService.java
+++ src/main/java/com/revature/wedding_planner/services/RentedResourceService.java
@@ -1,5 +1,35 @@
 package com.revature.wedding_planner.services;
 
-public class RentedResourceService {
+import java.util.List;
 
+import com.revature.wedding_planner.dao.RentedResourceDAO;
+import com.revature.wedding_planner.models.RentedResource;
+
+public class RentedResourceService {
+	private final RentedResourceDAO rentedResourceDAO;
+
+	public RentedResourceService(RentedResourceDAO rentedResourceDAO) {
+		this.rentedResourceDAO = rentedResourceDAO;
+	}
+
+	public boolean addRentedResource(RentedResource rentedResource) {
+		return rentedResourceDAO.addRentedResource(rentedResource);
+	}
+
+	public List<RentedResource> getAllRentedResources() {
+		return rentedResourceDAO.getAllRentedResources();
+	}
+
+	public RentedResource getRentedResourceByID(int id) {
+		return rentedResourceDAO.getRentedResourceByID(id);
+	}
+
+	public void updateRentedResourceWithSessionMethod(RentedResource rentedResource) {
+		rentedResourceDAO.updateRentedResourceWithSessionMethod(rentedResource);
+	}
+
+	// TODO implement this in DAO
+	public void deleteRentedResource(int id) {
+		rentedResourceDAO.deleteRentedResource(id);
+	}
 }
