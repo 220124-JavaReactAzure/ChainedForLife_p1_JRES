757e993fbda50b127dfdb068b37b7023f42f2ad1
aaescobar <47513774+aaescobar@users.noreply.github.com>
2022-02-23 18:04:12 -0500

    AuthenticationServlets DTOs and USer authentication methods updated

--- src/main/java/com/revature/wedding_planner/web/servlets/AuthServlet.java
+++ src/main/java/com/revature/wedding_planner/web/servlets/AuthServlet.java
@@ -1,7 +1,9 @@
 package com.revature.wedding_planner.web.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.revature.wedding_planner.dao.UserDAO;
 import com.revature.wedding_planner.exceptions.AuthenticationException;
 import com.revature.wedding_planner.exceptions.InvalidRequestException;
 import com.revature.wedding_planner.models.User;
@@ -25,33 +28,54 @@ public class AuthServlet extends HttpServlet{
 		this.userService = userService;
 		this.mapper = mapper;
 	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.getWriter().write("<h1>ChainedForLife</h1>\r\n"
+				+ "\r\n"
+				+ "	<h2> Wedding Planner Application Landing Page </h2>\r\n"
+				+ "\r\n"
+				+ "	<form action=\"auth\" method=\"post\">\r\n"
+				+ "		Username:<input type=\"text\" name=\"username\" /><br /><br />\r\n"
+				+ "		Password:<input type=\"password\" name=\"password\" /><br /><br />\r\n"
+				+ "		<input type=\"submit\" value=\"login\" />\r\n"
+				+ "	</form>");
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		try {
+		//try {
 			
-			// demo video week 2 recording 2
-			LoginCredentials loginCredentials = mapper.readValue(req.getInputStream(), LoginCredentials.class);
-			User authenticatedUser = userService.authenticateUser(loginCredentials.getUsername(), loginCredentials.getPassword());
+		resp.setContentType("text/html");
 		
-			HttpSession httpSession = req.getSession(false); //direct paths  if not loggin return null
+		PrintWriter writer = resp.getWriter();
+		
+		String usern = req.getParameter("email");
+		String userp = req.getParameter("password");
+		
+		
+		if(userService.authenticateUser(usern, userp) != null) {
+			RequestDispatcher rd = req.getRequestDispatcher("user");
+			rd.forward(req, resp);
 			
-			httpSession.setAttribute("authUser", authenticatedUser);
-		} catch (InvalidRequestException e) {
-			resp.setStatus(400);
-		} catch (AuthenticationException e) {
-			resp.setStatus(401);
-		} catch (Exception e) {
-			e.printStackTrace();
-			resp.setStatus(500);
+			
+		} else {
+			
+			writer.print("Sorry username or password incorrect go head and try again..");
+			RequestDispatcher rd = req.getRequestDispatcher("index.html");
+			rd.include(req, resp);
+		
 		}
+		//User authenticatedUser = userService.authenticateUser(loginCredentials.getUsername(), loginCredentials.getPassword());
 		
-		
+		//} finally {
+			
 		
 	}
 	
-	
-
 }
+
+
